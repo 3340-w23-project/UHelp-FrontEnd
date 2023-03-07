15dd5622b301e9719fadde60042d8aa83e5178f8
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-03-07 02:59:23 -0500

    update: Logo & Color Palette

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,18 +2,25 @@ import styles from "@/styles/Navbar.module.scss";
 import Link from "next/link";
 import { MenuItem } from "./MenuItem";
 
+const menuItems = [
+  { label: "Home", href: "/" },
+  { label: "Sign Up", href: "/signup" },
+];
+
 const Navbar = () => (
   <div className={styles.navbar}>
     <div className={styles.logo}>
       <Link href="/">
         <img src="/logo.png" alt="UHelp" width={50} height={50} />
+        UHelp
       </Link>
     </div>
 
     <nav>
       <ul className={styles.menu}>
-        <MenuItem label="Home" href="/" />
-        <MenuItem label="Sign In" href="/" />
+        {menuItems.map((item) => (
+          <MenuItem key={item.label} {...item} />
+        ))}
       </ul>
     </nav>
   </div>
