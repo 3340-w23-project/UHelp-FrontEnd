034e57859ecb10b1cac08f243a454f5236b7f7bb
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-03-08 02:59:09 -0500

    refactor: Homepage - Extracted data to a separate file - Used a map function to render the sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Section } from "@/components/Homepage/Section";
 import { Footer } from "@/components/Footer";
 import HeroSection from "@/components/Homepage/HeroSection";
 import MetaTags from "@/components/MetaTags";
+import { sections } from "@/utils/HomeData";
 
 type HomeProps = {
   isScrolled: boolean;
@@ -20,27 +21,18 @@ export default function Home({ isScrolled, isMobile }: HomeProps) {
           actionLabel="Join Now"
           actionHref="/signup"
         />
-        <Section
-          isMobile={isMobile}
-          heading="Connect with Classmates and Get Help"
-          description="Find academic assistance and connect with peers by joining a class-specific forum. Get multiple perspectives on course material and discover new study techniques to help you succeed in your classes."
-          image="/images/img1.png"
-        />
-        <Section
-          isMobile={isMobile}
-          heading="Find Answers Quickly and Easily"
-          description="Use our efficient and user-friendly platform to quickly find answers to common questions. Browse through threads or use our search function to find the information you need. Follow our guidelines to keep the community organized and helpful."
-          image="/images/img2.png"
-          reverse
-        />
-        <Section
-          isMobile={isMobile}
-          heading="Join a Supportive Community of Learners"
-          description="Be part of a safe and respectful community of learners from diverse backgrounds and perspectives. Connect with others, get academic support, and participate in additional resources and events like study groups and online workshops."
-          image="/images/img3.png"
-          buttonLabel="Get Started"
-          buttonHref="/signup"
-        />
+        {sections.map((section, index) => (
+          <Section
+            key={index}
+            heading={section.heading}
+            description={section.description}
+            buttonLabel={section.buttonLabel}
+            buttonHref={section.buttonHref}
+            image={section.image}
+            reverse={section.reverse}
+            isMobile={isMobile}
+          />
+        ))}
         <Footer />
       </main>
     </>
---
+++ src/utils/HomeData.ts
@@ -0,0 +1,23 @@
+export const sections = [
+  {
+    heading: "Connect with Classmates and Get Help",
+    description:
+      "Find academic assistance and connect with peers by joining a class-specific forum. Get multiple perspectives on course material and discover new study techniques to help you succeed in your classes.",
+    image: "/images/img1.png",
+  },
+  {
+    heading: "Find Answers Quickly and Easily",
+    description:
+      "Use our efficient and user-friendly platform to quickly find answers to common questions. Browse through threads or use our search function to find the information you need. Follow our guidelines to keep the community organized and helpful.",
+    image: "/images/img2.png",
+    reverse: true,
+  },
+  {
+    heading: "Join a Supportive Community of Learners",
+    description:
+      "Be part of a safe and respectful community of learners from diverse backgrounds and perspectives. Connect with others, get academic support, and participate in additional resources and events like study groups and online workshops.",
+    image: "/images/img3.png",
+    buttonLabel: "Get Started",
+    buttonHref: "/signup",
+  },
+];
