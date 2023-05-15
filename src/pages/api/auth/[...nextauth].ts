74191aafd44ae4ee9057243d7a4ec95eaed1c732
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-05-14 23:59:46 -0400

    fix: Next-auth Api Error on Authorize

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,7 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/signin",
     newUser: "/signup",
+    error: "/signin",
   },
   providers: [
     CredentialsProvider({
@@ -20,25 +21,29 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/signin`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: credentials?.username,
-            password: credentials?.password,
-          }),
-        });
-        const user = await res.json();
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        try {
+          const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/signin`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: credentials?.username,
+              password: credentials?.password,
+            }),
+          });
+          const user = await res.json();
+          if (res.ok && user) {
+            return user;
+          }
+          //return params to sign in page
+          return Promise.reject(
+            new Error(user?.msg || "Something went wrong, please try again")
+          );
+        } catch (error) {
+          return Promise.reject(
+            new Error("Something went wrong, please try again")
+          );
         }
       },
     }),
