6aa45c66c805c9789c37da992c6ec050ceb24328
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-06-01 13:21:02 -0400

    feat: Close Modal Shortcut

--- src/hooks/useShortcuts.tsx
+++ src/hooks/useShortcuts.tsx
@@ -1,13 +1,18 @@
+import { setIsOpen } from "@/redux/slices/forumSlice";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export default function useSubmitShortcut(
   onSubmit: () => void,
   status: boolean
 ) {
+  const dispatch = useDispatch();
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (status && e.key === "Enter" && e.ctrlKey) {
         onSubmit();
+      } else if (status && e.key === "Escape") {
+        dispatch(setIsOpen(false));
       }
     };
     window.addEventListener("keydown", handleKeyDown);
